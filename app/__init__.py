2d3c7aebf6bffd9db16722fac567b88c2118cdb3
Ellie <elysiarosalind@gmail.com>
2017-07-17 00:28:38 -0400

    Had to vendor the firebase module to resolve the dependency issue, working to resolve build issues

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
+import os
+import sys
+
+# Add vendor directory to module search path
+parent_dir = os.path.abspath(os.path.dirname(__file__))
+vendor_dir = sys.path.append(parent_dir + '/vendor')
+print(vendor_dir)
+print(sys.path)
 from firebase import firebase
 
 firebase_db = firebase.FirebaseApplication(app.config["FIREBASE_DB_URL"][0], None)
